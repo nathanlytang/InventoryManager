e19792183a0116f7c307d8dab19f0e3464994e49
Nathan Tang <natt888@yahoo.ca>
2019-05-22 00:10:12 -0600

    Open file

--- functions.py
+++ functions.py
@@ -4,6 +4,10 @@ Nathan Tang
 19/05/16
 '''
 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+
 class items:
     def __init__(self, item, ID, price, totAvail, totChecked, description):
         self.item = None
@@ -20,3 +24,6 @@ class totalAvailable:
 class totalChecked:
     def __init__(self):
         self.totChecked = None
+
+def openFile():
+    filedialog.askopenfilename(initialdir="/", filetypes=(("CSV", "*.csv"),("All Files", "*.*")))
